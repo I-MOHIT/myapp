47e75cfacc82f69d41850715c5d420ea7c0bbd82
I-MOHIT <mohitchugh180919@gmail.com>
2021-11-03 19:25:36 +0530

    added use of few other http methods
    Signed-off-by: I-MOHIT <mohitchugh180919@gmail.com>

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get('/',function(req,res){
     res.send('Hello World!')
 })
 
+app.post('/',function(req,res){
+    res.send('Got a POST request')
+})
+
+app.put('/user',function(req,res){
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user',function(req,res){
+    res.send('Got a DELETE request at /user')
+})
+
 app.listen(port,function(){
     console.log(`example app listening at http://localhost:${port}`)
 })
